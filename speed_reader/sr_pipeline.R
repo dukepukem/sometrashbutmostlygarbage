44601121e62258b804055c118aa5fc74fd2e43f9
dukepukem <127468498+dukepukem@users.noreply.github.com>
2023-03-10 16:21:02 +1100

    Update sr_pipeline.R

---
+++ speed_reader/sr_pipeline.R
@@ -0,0 +1,18 @@
+# Load Libraries ----------------------------------------------------------
+# Needed for Speed Reader 
+library(vctrs)
+library(data.table)
+library(stringr)
+library(shiny)
+library(jsonlite)
+library(sourcetools)
+library(shinydashboard)
+library(ggplot2)
+library(gganimate)
+
+# Not needed but still cool
+# library(lubridate)
+# library(janitor)
+# library(zoo)
+# library(dplyr)
+# library(tidyr)
